481da743a347ea44a1100718e93cdc85c8cc79bf
wangrui <wr103750@163.com>
2021-08-09 14:20:29 +0800

    z版本测试

--- renderer.js
+++ renderer.js
@@ -5,10 +5,17 @@ const CLICK_MESSAGE = 'Notification clicked'
 new Notification(NOTIFICATION_TITLE, { body: NOTIFICATION_BODY }).onclick = () => console.log(CLICK_MESSAGE)
 
 $(function(){
+    $(function (){
+        $("#span_select").hide()
+    });
     $("#b_ceshi_kd").click(function (){
         window.context.accountTest($("#i_username").val(),$("#i_password").val(),"kd");
     });
     $("#b_ceshi_nmy").click(function (){
         window.context.accountTest($("#i_username").val(),$("#i_password").val(),"nmy");
+    });
+    $("#b_open_new_window").click(function (){
+        $("#span_login").hide();
+        $("#span_select").show();
     });
 });
